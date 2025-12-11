1d5df2010592e7ff46c8fd51848b92bc2d3c2f7d
Jay Lox <letmisitheshow@gmail.com>
2025-12-10 22:45:15 -0800

    Initial SPFx scaffolding

---
+++ src/webparts/apiExtensionViewer/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IApiExtensionViewerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ApiExtensionViewerWebPartStrings' {
+  const strings: IApiExtensionViewerWebPartStrings;
+  export = strings;
+}
